ec855a6d514ceec1ca6b8167d3b3e14a7202486e
ben.chan <ben.chan@imbee.io>
2019-11-28 00:03:59 +0800

    Updates

---
+++ precache-manifest.e84c829199a5b9af5d8ef615498cb928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b43bd1c9075137f131593bc45a2394",
+    "url": "/tertis-challenge/index.html"
+  },
+  {
+    "revision": "e2dd9e650d2de382e3ac",
+    "url": "/tertis-challenge/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "13fe561cc5202927ea2f",
+    "url": "/tertis-challenge/static/js/2.0ac01b75.chunk.js"
+  },
+  {
+    "revision": "e2dd9e650d2de382e3ac",
+    "url": "/tertis-challenge/static/js/main.b4530c38.chunk.js"
+  },
+  {
+    "revision": "0fc27b979346bac36f4b",
+    "url": "/tertis-challenge/static/js/runtime-main.c59b8147.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/tertis-challenge/static/media/logo.25bf045c.svg"
+  }
+]);
